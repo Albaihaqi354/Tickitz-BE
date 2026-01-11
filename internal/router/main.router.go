7062616c6bb225980bfee1ace6039f5d5489482d
Muhammad Bian Al Baihaqi <bianalbaihaqi190@gmail.com>
2026-01-11 23:59:09 +0700

    feat[Route] Create feature Route

--- internal/router/main.router.go
+++ internal/router/main.router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/Albaihaqi354/Tickitz-BE/internal/controller"
+	"github.com/Albaihaqi354/Tickitz-BE/internal/middleware"
 	"github.com/Albaihaqi354/Tickitz-BE/internal/repository"
 	"github.com/Albaihaqi354/Tickitz-BE/internal/service"
 	"github.com/gin-gonic/gin"
@@ -9,6 +10,10 @@ import (
 )
 
 func Init(app *gin.Engine, db *pgxpool.Pool) {
+	adminRepository := repository.NewAdminRepository(db)
+	adminService := service.NewAdminService(adminRepository)
+	adminController := controller.NewAdminController(adminService)
+
 	userRepository := repository.NewUserRepository(db)
 	userService := service.NewUserService(userRepository)
 	userController := controller.NewUserController(userService)
@@ -19,5 +24,14 @@ func Init(app *gin.Engine, db *pgxpool.Pool) {
 
 	app.POST("/auth/register", userController.AddUser)
 	app.POST("/auth/login", userController.Login)
+
 	app.GET("/movies/upcoming", movieController.GetUpcomingMovies)
+	app.GET("/movies/popular", movieController.GetPopularMovie)
+	app.GET("/movies/filter", movieController.GetMovieWithFilter)
+	app.GET("/movies/detail", movieController.GetMovieDetail)
+
+	app.GET("/admin/getMovies", middleware.VerifyToken, middleware.CheckRole("admin"), adminController.GetAllMovieAdmin)
+	app.DELETE("/admin/movies/:id", middleware.VerifyToken, middleware.CheckRole("admin"), adminController.DeleteMovieAdmin)
+
+	app.GET("/user/profile", middleware.VerifyToken, middleware.CheckRole("user"), userController.GetProfile)
 }
