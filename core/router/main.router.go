9a1353d065f0b796bf8e034ff13b0b115429425b
Muhammad Bian Al Baihaqi <bianalbaihaqi190@gmail.com>
2026-03-19 02:59:04 +0700

    chore: prepare for deployment (CORS & Env updates)

--- core/router/main.router.go
+++ core/router/main.router.go
@@ -7,12 +7,34 @@ import (
 	"github.com/redis/go-redis/v9"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
+	"context"
 )
 
 func Init(app *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
 	app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	app.Static("/profile", "./public/profile")
 	app.Static("/movie", "./public/movie")
+
+	app.GET("/health", func(c *gin.Context) {
+		dbErr := db.Ping(context.Background())
+		dbStatus := "OK"
+		if dbErr != nil {
+			dbStatus = "Error: " + dbErr.Error()
+		}
+
+		rdbStatus := "OK"
+		rdbErr := rdb.Ping(context.Background()).Err()
+		if rdbErr != nil {
+			rdbStatus = "Error: " + rdbErr.Error()
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"database": dbStatus,
+			"redis":    rdbStatus,
+			"status":   "Backend is running",
+		})
+	})
 
 	RegisterAuthRouter(app, db, rdb)
 	RegisterMovieRouter(app, db, rdb)
